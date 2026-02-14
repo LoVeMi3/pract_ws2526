5c2e2759487c3bae58ba08ff973a71b379651f1d
LoVeMi3 <l.vera.2023@alumnos.urjc.es>
2026-02-14 21:19:45 +0100

    toca probarlo

---
+++ build/practica2/CMakeFiles/teleopt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teleopt.dir/src/teleopt_main.cpp.o"
+  "CMakeFiles/teleopt.dir/src/teleopt_main.cpp.o.d"
+  "teleopt"
+  "teleopt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/teleopt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
