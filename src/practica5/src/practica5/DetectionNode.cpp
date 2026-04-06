56edf32b06abd38c70feb16dd21dd5764c7b303e
LoVeMi3 <l.vera.2023@alumnos.urjc.es>
2026-04-07 01:15:31 +0200

    no hay header de bt_examples, necesito eso, qué hago, hay que pedírselo al profe, y voy trágicamente tarde, what is life

--- src/practica5/src/practica5/DetectionNode.cpp
+++ src/practica5/src/practica5/DetectionNode.cpp
@@ -7,10 +7,15 @@
 #include <random>
 #include <string>
 
+#include "practica5/DetectionNode.hpp"
+
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "std_msgs/msg/bool.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
 
 // executable='detection' VERYIMPORTANT
 
